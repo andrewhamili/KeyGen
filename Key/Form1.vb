9113f584d6f692ac9a5d3ff5e1cbc374f9151ce5
andrewhamili <rockr1521@gmail.com>
2017-06-08 16:17:13 +0800

    Can now generate a 32-digit key from hardware ID's

--- Key/Form1.vb
+++ Key/Form1.vb
@@ -2,39 +2,18 @@
 Imports System
 Imports System.Text
 Imports System.Security.Cryptography
+Imports System.Net.NetworkInformation
 
 
 Public Class Form1
     Public kk As String = ""
     Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim hw As New clsComputerInfo
-
-        Dim hdd As String
-        Dim cpu As String
-        Dim mb As String
-        Dim mac As String
-
-        cpu = hw.GetProcessorId()
-        hdd = hw.GetVolumeSerial("C")
-        mb = hw.GetMotherBoardID()
-        mac = hw.GetMACAddress()
-
-
-        MsgBox(cpu & "   " & hdd & "   " & mb & "   " & mac)
-
-
-        Dim hwid As String = Strings.UCase(hw.getMD5Hash(cpu & hdd & mb))
-
-        ' MessageBox.Show(Strings.UCase(hwid))
-
-        TextBox1.Text = hwid
-        kk = hwid
-        CheckLicense()
+        StartActivity()
     End Sub
 
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Me.Close()
+        StartActivity()
     End Sub
     Public Sub CheckLicense()
         If kk = "08F6ADA116C6946AC2D8B168B6F78FC1" Then
@@ -43,8 +22,41 @@ Public Class Form1
             MsgBox("License did not match")
         End If
     End Sub
-End Class
 
+    Public Sub StartActivity()
+        Dim hw As New clsComputerInfo
+
+        Dim hdd As String
+        Dim cpu As String
+        Dim mb As String
+        Dim mac As String
+        Dim video As String
+
+        cpu = hw.GetProcessorId()
+        hdd = hw.GetVolumeSerial("C")
+        mb = hw.GetMotherBoardID()
+
+        mac = hw.GetMACAddress()
+
+
+
+
+        video = hw.GetVideoId()
+
+
+        MsgBox(cpu & "   " & hdd & "   " & mb & "   " & mac & "   " & video)
+
+
+        Dim hwid As String = Strings.UCase(hw.getMD5Hash(cpu & hdd & mb & mac & video))
+
+        ' MessageBox.Show(Strings.UCase(hwid))
+
+        TextBox1.Text = hwid
+        kk = hwid
+        CheckLicense()
+    End Sub
+
+End Class
 
 Public Class clsComputerInfo
 
@@ -60,21 +72,51 @@ Public Class clsComputerInfo
         Return strProcessorId
 
     End Function
+    Friend Function GetVideoId() As String
+        'Dim strProcessorId As String = String.Empty
+        'Dim query As New SelectQuery("Win32_video")
+        'Dim search As New ManagementObjectSearcher(query)
+        'Dim info As ManagementObject
+
+        'For Each info In search.Get()
+        '    strProcessorId = info("videoId").ToString()
+        'Next
+        'Return strProcessorId
+        Dim VideoId As String = String.Empty
+
+        Dim mc As New System.Management.ManagementClass("Win32_VideoController")
+        Dim moc As System.Management.ManagementObjectCollection
+
+        moc = mc.GetInstances()
+
+
+
+
+
+    End Function
 
     Friend Function GetMACAddress() As String
 
-        Dim mc As ManagementClass = New ManagementClass("Win32_NetworkAdapterConfiguration")
-        Dim moc As ManagementObjectCollection = mc.GetInstances()
+        'Dim mc As ManagementClass = New ManagementClass("Win32_NetworkAdapterConfiguration")
+        'Dim moc As ManagementObjectCollection = mc.GetInstances()
         Dim MACAddress As String = String.Empty
-        For Each mo As ManagementObject In moc
+        'Dim MACAddress2 As String = String.Empty
+        'Dim tempstore As New List(Of String)
+        'For Each mo As ManagementObject In moc
 
-            If (MACAddress.Equals(String.Empty)) Then
-                If CBool(mo("IPEnabled")) Then MACAddress = mo("MacAddress").ToString()
+        '    If (MACAddress.Equals(String.Empty)) Then
+        '        'If CBool(mo("IPEnabled")) Then
+        '        MACAddress = mo("MacAddress").ToString()
+        '    End If
+        '    'MACAddress = MACAddress.Replace(":", String.Empty)
 
-                mo.Dispose()
+        'Next
+
+        Dim nics() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces
+        For i As Integer = 0 To nics.Count - 1
+            If nics(i).OperationalStatus = OperationalStatus.Up Then
+                MACAddress += nics(i).GetPhysicalAddress.ToString()
             End If
-            MACAddress = MACAddress.Replace(":", String.Empty)
-
         Next
         Return MACAddress
     End Function
